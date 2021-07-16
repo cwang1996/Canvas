5ebf477a1d1f0cebd489af244131614bf72ccb84
Calvin Wang <calvinwang@yahoo.com>
2021-07-16 13:49:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+const container = document.querySelector('.container');
+const red_btn = document.querySelector('.red');
+const blue_btn = document.querySelector('.blue');
+const green_btn = document.querySelector('.green');
+const delete_btn = document.querySelector('.clear');
+
+let stroke_color = 'black';
+
 window.addEventListener('load', () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
 
     //Resizing
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+    // canvas.height = canvas.clientHeight;
+    // canvas.width = canvas.clientWidth;
 
     //variables
     let painting = false;
@@ -21,15 +29,31 @@ window.addEventListener('load', () => {
 
     function draw(e){
         if(!painting) return;
-        ctx.lineWidth = 10;
+        ctx.lineWidth = 5;
         ctx.lineCap = 'round';
-        ctx.strokeStyle = 'red';
+        ctx.strokeStyle = stroke_color;
 
-        ctx.lineTo(e.clientX, e.clientY);
+        ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
+        ctx.moveTo(e.offsetX, e.offsetY);
     }
+
+    red_btn.addEventListener('click', function(){
+        stroke_color = 'red';
+    })
+
+    blue_btn.addEventListener('click', function(){
+        stroke_color = 'blue';
+    })
+
+    green_btn.addEventListener('click', function(){
+        stroke_color = 'green';
+    })
+
+    delete_btn.addEventListener('click', function(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    })
 
     //EventListeners 
     canvas.addEventListener('mousedown', startPosition);
